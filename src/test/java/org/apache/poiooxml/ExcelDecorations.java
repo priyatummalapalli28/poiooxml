1c43bfe131b00837c2a692d04c649cd814e04f80
gattu <gattu@DESKTOP-D8HNM4S>
2020-11-10 01:36:21 +0530

    prototype programs using poiooxml

---
+++ src/test/java/org/apache/poiooxml/ExcelDecorations.java
@@ -0,0 +1,27 @@
+package org.apache.poiooxml;
+
+
+public class ExcelDecorations
+{
+	public static void main(String[] args)
+	{
+		/*//create font
+        Font font=wb.createFont();
+        font.setFontHeightInPoints((short)10);
+        font.setFontName("Arial");
+        font.setColor(IndexedColors.WHITE.getIndex());
+        font.setBold(true);
+        font.setItalic(true);
+                                    
+        //Create cell style 
+        CellStyle style=workbook.createCellStyle();;
+        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        
+        // Setting font to style
+        style.setFont(font);
+        
+        // Setting cell style
+        cell.setCellStyle(style);*/
+	}
+}
